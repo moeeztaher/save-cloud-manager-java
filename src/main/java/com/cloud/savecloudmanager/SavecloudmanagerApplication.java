699e78f32a416c05eaa00c51e37dd11568327322
Moeez Tahir <moeztahir96@gmail.com>
2023-11-18 09:04:18 +0100

    first commit

---
+++ src/main/java/com/cloud/savecloudmanager/SavecloudmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.savecloudmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavecloudmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavecloudmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/savecloudmanager/SavecloudmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.savecloudmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavecloudmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
